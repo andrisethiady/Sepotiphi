48bd0bbf71ee7a0d0e2e4cd86abe6985026c2a38
andri <andri.setiady@indomaret.co.id>
2024-10-10 14:59:54 +0700

    added playable music and play music function

---
+++ Sepothipi/Cores/Enum/PlayableSongEnum.swift
@@ -0,0 +1,16 @@
+//
+//  PlayableSongEnum.swift
+//  Sepothipi
+//
+//  Created by Andri on 10/10/24.
+//
+
+import Foundation
+
+enum PlayableSongEnum {
+    static let aboutYou = "AboutYou"
+    static let enchanted = "Enchanted"
+    static let endOfBegining = "EndOfBegining"
+    static let foreverYoung = "ForeverYoung"
+    static let HidupHarusTetapBerjalan = "HidupHarusTetapBerjalan"
+}
--- Sepothipi/Module/Home/Views/HomeViewController.swift
+++ Sepothipi/Module/Home/Views/HomeViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class HomeViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    var timer: Timer? = nil
 
     @IBOutlet weak var tableViewHeader: UIView!
     
@@ -21,23 +25,77 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupUI()
+        setMusic(path: PlayableSongEnum.enchanted)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        timer?.invalidate()
+    }
+    
+    func setupUI() {
+        tableViewHeader.roundCorners(corners: [.topLeft, .topRight], radius: 16)
+    }
+    
+    func setupBinding() {
+        
+    }
+    
+    func setMusic(path: String?) {
+        guard path != nil else { return }
+        
+        let url = Bundle.main.url(forResource: path, withExtension: "mp3")
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url!)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateSlider), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateSlider() {
+        progressBar.value = Float(player?.currentTime ?? 0)
+        
+        if player.isPlaying == true {
+            btnPlay.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        } else {
+            btnPlay.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        }
     }
 
     @IBAction func onPressBtnPrev(_ sender: Any) {
+        
     }
     
     @IBAction func onPressBtnNext(_ sender: Any) {
+        
     }
     
     @IBAction func onPressBtnPlay(_ sender: Any) {
+        if player.isPlaying == true {
+            player.stop()
+        } else {
+            player.play()
+            progressBar.maximumValue = Float(player.duration)
+        }
     }
     
     @IBAction func onPressAllSong(_ sender: Any) {
+        
     }
     
     @IBAction func onPressPlayableSong(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onProgressBarValueChange(_ sender: Any) {
+        player.stop()
+        player.currentTime = TimeInterval(progressBar.value)
+        player.prepareToPlay()
+        player.play()
     }
     
     
